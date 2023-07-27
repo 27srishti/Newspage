233eb8ab26804ad350aef7d0b23c27029359b634
srishti suratkal <279srishti@gmail.com>
2023-07-27 23:01:41 +0530

    upd-17

--- src/app/Components/Head.js
+++ src/app/Components/Head.js
@@ -2,7 +2,6 @@ import React from "react";
 export const Head = () => {
   return (
     <div className="container mx-auto  overflow-x-hidden mt-6">
-     
       <div className="flex flex-row flex-wrap ">
         <div className="w-full   md:w-[69%] h-64 md:h-auto md:mb-4 mb-2 ">
           <a className="relative ">
@@ -35,12 +34,12 @@ export const Head = () => {
           <div className="flex  flex-row md:flex-col  md:-mx-2">
             <div className="w-full w-1/2 pr-2 md:w-full h-48 xl:h-64 md:mb-2  md:mb-2 md:px-2">
               <a className="relative">
-                
                 <img
                   className="block w-full h-full  bg-no-repeat bg-center bg-cover "
-                  src="/images/Head 2.avif"
+                  src="/images/Body-3.avif"
                   alt="H1"
-                /><div className="bg-black opacity-50 w-full h-full  absolute bottom-0 px-5"></div>
+                />
+                <div className="bg-black opacity-50 w-full h-full  absolute bottom-0 px-5"></div>
                 <div className=" absolute bottom-1 px-5 ">
                   <div className="bg-neutral-600 w-32 mb-2 hover:bg-yellow-300 hover:text-black px-2 py-1.5 text-center text-[0.65rem] md:text-[0.70rem] rounded text-white">
                     Opinion & Analysis
@@ -49,7 +48,7 @@ export const Head = () => {
                     href="https://news.networktigers.com/opinion/could-the-kronos-hack-have-been-prevented/"
                     className="  text-gray-100 font-bold text-sm md:text-xl  capitalize"
                   >
-                    Cybersecurity news weekly roundup 
+                    Cybersecurity news weekly roundup
                   </a>
                   <div className="flex pb-3  ">
                     <div className="text-[0.75rem] text-white text-left py-1.5 font-medium">
@@ -61,13 +60,13 @@ export const Head = () => {
               </a>
             </div>
             <div className="w-full w-1/2  md:w-full h-48 xl:h-64  md:px-2">
-            <a className="relative">
-                
+              <a className="relative">
                 <img
                   className="block w-full h-full  bg-no-repeat bg-center bg-cover "
                   src="/images/Maximizing ROI on network equipment.avif"
                   alt="H1"
-                /><div className="bg-black opacity-50 w-full h-full  absolute bottom-0 px-5"></div>
+                />
+                <div className="bg-black opacity-50 w-full h-full  absolute bottom-0 px-5"></div>
                 <div className=" absolute bottom-1 px-5 ">
                   <div className="bg-neutral-600 w-32 mb-2 hover:bg-yellow-300 hover:text-black px-2 py-1.5 text-center text-[0.65rem] md:text-[0.70rem] rounded text-white">
                     Opinion & Analysis
--- src/app/Components/ItemCard.js
+++ src/app/Components/ItemCard.js
@@ -1,6 +1,8 @@
 // components/ItemCard.js
 // @client
 "use client";
+import React from 'react';
+
 const ItemCard = ({
   title,
   imageUrl,
@@ -11,23 +13,23 @@ const ItemCard = ({
   readMore,
 }) => {
   return (
-    <div className="w-full ">
+    <div className="w-full">
       <a
         className="block rounded w-full lg:flex mb-10"
-        href="./blog-single-1.html"
+        href={readMore} // Use the provided 'readMore' prop as the link destination
+        target="_blank" // Open link in a new tab
+        rel="noopener noreferrer" // Recommended for security reasons when using 'target="_blank"'
       >
         <div>
-          <a>
-            <img
-              className="w-full h-80 md:w-full lg:w-[30rem] mb-5 md:mb-5 lg:mb-0  lg:h-[15rem] object-cover md:rounded "
-              src={imageUrl}
-              alt={title}
-            />
-          </a>
+          <img
+            className="w-full h-80 md:w-full lg:w-[30rem] mb-5 md:mb-5 lg:mb-0 lg:h-[15rem] object-cover md:rounded"
+            src={imageUrl}
+            alt={title}
+          />
         </div>
         <div className="bg-white rounded pl-5 lg:pl-5 md:pl-0 flex w-full flex-col justify-evenly leading-normal">
-          <div className="">
-            <a className="mt-3  lg:mt-0 hover:text-lime-600 text-gray-700 font-bold text-2xl  mb-2">
+          <div>
+            <a className="mt-3 lg:mt-0 hover:text-lime-600 text-gray-700 font-bold text-2xl mb-2">
               {title}
             </a>
             <div className="flex pb-6 pt-1">
@@ -38,16 +40,15 @@ const ItemCard = ({
                 {name} - <span className="font-light">{date}</span>
               </div>
             </div>
-            <p className="text-gray-500 w-[90%]  text-base">{description}</p>
+            <p className="text-gray-500 w-[90%] text-base">{description}</p>
           </div>
           <div className="flex mt-3">
             <a
               href={readMore}
-              className=" px-1 py-2 w-24 md:px-2 md:py-2 md:w-28 text-sm md:text-base  text-center rounded text-white cursor-pointer bg-lime-600 hover:bg-neutral-700"
+              className="px-1 py-2 w-24 md:px-2 md:py-2 md:w-28 text-sm md:text-base text-center rounded text-white cursor-pointer bg-lime-600 hover:bg-neutral-700"
             >
               Read More
             </a>
-            
           </div>
         </div>
       </a>
