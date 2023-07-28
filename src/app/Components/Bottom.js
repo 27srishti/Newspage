f6a64c7ea97c5988a95b566cc5dc998a6743c31a
srishti suratkal <279srishti@gmail.com>
2023-07-28 16:42:07 +0530

    upd-28

--- src/app/Components/Bottom.js
+++ src/app/Components/Bottom.js
@@ -58,7 +58,8 @@ export const Bottom = () => {
             <ul className="flex items-center  lg:mr-6 xl:mr-8">
               <li className="p-1">
                 <a
-                  href="https://www.facebook.com/networktigers/"
+                  aria-label="Twitter"
+                  href="https://twitter.com/networktigers"
                   className="inline-block rounded-full  p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5"
                 >
                   <svg
@@ -81,7 +82,8 @@ export const Bottom = () => {
               </li>
               <li className="p-1">
                 <a
-                  href="https://www.linkedin.com/company/networktigers/"
+                  aria-label="Facebook"
+                  href="https://www.facebook.com/networktigers/"
                   className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5"
                 >
                   <svg
@@ -104,7 +106,8 @@ export const Bottom = () => {
               </li>
               <li className="p-1">
                 <a
-                  href="https://twitter.com/networktigers"
+                  aria-label="LinkedIn"
+                  href="https://www.linkedin.com/company/networktigers/"
                   className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5"
                 >
                   <svg
@@ -337,132 +340,131 @@ export const Bottom = () => {
 
         {/* ----------------------------4th column ----------------------------------------- */}
         <div className="py-5 pb-2 xl:col-span-3 md:col-span-12 col-span-12 lg:block">
-  <div>
-    <div className="pt-5 pb-3">
-      <div className="relative flex items-center mb-3 text-gray-600 hover:text-blue-400 cursor-pointer hover:text-gray-600 hover:text-blue-500">
-        <div className="relative">
-          <div className="absolute bottom-0 text-center inline-block w-40 xl:w-40 py-2 text-white font-medium bg-orange-700 rounded">
-            <span className="inline-block text-sm text-center leading-tight">
-              Popular Categories
-            </span>
-            <span
-              className="absolute bottom-0 right-0 w-3 h-3 -mb-1 transform rotate-45 bg-orange-700"
-              style={{ left: "50%" }}
-            />
+          <div>
+            <div className="pt-5 pb-3">
+              <div className="relative flex items-center mb-3 text-gray-600 hover:text-blue-400 cursor-pointer hover:text-gray-600 hover:text-blue-500">
+                <div className="relative">
+                  <div className="absolute bottom-0 text-center inline-block w-40 xl:w-40 py-2 text-white font-medium bg-orange-700 rounded">
+                    <span className="inline-block text-sm text-center leading-tight">
+                      Popular Categories
+                    </span>
+                    <span
+                      className="absolute bottom-0 right-0 w-3 h-3 -mb-1 transform rotate-45 bg-orange-700"
+                      style={{ left: "50%" }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <ul>
+              <li className="px-1  transition duration-300">
+                <a
+                  href="https://news.networktigers.com/category/all-articles/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  All Articles
+                  <span
+                    href="https://news.networktigers.com/category/all-articles/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    563
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>
+              <li className="px-1   transition duration-300">
+                <a
+                  href="https://news.networktigers.com/category/industry-news/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  Industry News
+                  <span
+                    href="https://news.networktigers.com/category/industry-news/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto "
+                  >
+                    355
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>
+              <li className="px-1 ">
+                <a
+                  href="https://news.networktigers.com/category/opinion/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  Opinion & Analysis
+                  <span
+                    href="https://news.networktigers.com/category/opinion/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    278
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>
+              <li className="px-1 ">
+                <a
+                  href="https://news.networktigers.com/category/cybersecurity-news/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  Cybersecurity News
+                  <span
+                    href="https://news.networktigers.com/category/cybersecurity-news/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    191
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>
+              <li className="px-1 ">
+                <a
+                  href="https://news.networktigers.com/category/networktigers-news/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  NetworkTigers News
+                  <span
+                    href="https://news.networktigers.com/category/networktigers-news/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    135
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>{" "}
+              <li className="px-1 ">
+                <a
+                  href="https://news.networktigers.com/category/network-news/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  Network News
+                  <span
+                    href="https://news.networktigers.com/category/network-news/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    100
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>{" "}
+              <li className="px-1 ">
+                <a
+                  href="https://news.networktigers.com/category/remote-work/"
+                  className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
+                >
+                  Remote Work
+                  <span
+                    href="https://news.networktigers.com/category/remote-work/"
+                    className="text-gray-600 hover:text-blue-400 ml-auto"
+                  >
+                    61
+                  </span>
+                  <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </div>
-    <ul>
-      <li className="px-1  transition duration-300">
-        <a
-          href="https://news.networktigers.com/category/all-articles/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          All Articles
-          <span
-            href="https://news.networktigers.com/category/all-articles/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            563
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>
-      <li className="px-1   transition duration-300">
-        <a
-          href="https://news.networktigers.com/category/industry-news/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          Industry News
-          <span
-            href="https://news.networktigers.com/category/industry-news/"
-            className="text-gray-600 hover:text-blue-400 ml-auto "
-          >
-            355
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>
-      <li className="px-1 ">
-        <a
-          href="https://news.networktigers.com/category/opinion/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          Opinion & Analysis
-          <span
-            href="https://news.networktigers.com/category/opinion/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            278
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>
-      <li className="px-1 ">
-        <a
-          href="https://news.networktigers.com/category/cybersecurity-news/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          Cybersecurity News
-          <span
-            href="https://news.networktigers.com/category/cybersecurity-news/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            191
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>
-      <li className="px-1 ">
-        <a
-          href="https://news.networktigers.com/category/networktigers-news/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          NetworkTigers News
-          <span
-            href="https://news.networktigers.com/category/networktigers-news/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            135
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>{" "}
-      <li className="px-1 ">
-        <a
-          href="https://news.networktigers.com/category/network-news/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          Network News
-          <span
-            href="https://news.networktigers.com/category/network-news/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            100
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>{" "}
-      <li className="px-1 ">
-        <a
-          href="https://news.networktigers.com/category/remote-work/"
-          className="flex items-center text-gray-600 hover:text-blue-500 cursor-pointer"
-        >
-          Remote Work
-          <span
-            href="https://news.networktigers.com/category/remote-work/"
-            className="text-gray-600 hover:text-blue-400 ml-auto"
-          >
-            61
-          </span>
-          <i className="text-gray-600 hover:text-blue-400 bx bx-right-arrow-alt ml-1" />
-        </a>
-      </li>
-    </ul>
-  </div>
-</div>
-
       </div>
     </section>
   );
